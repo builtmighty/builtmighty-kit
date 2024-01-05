380ffcc99edc41b7b82fe7d5470a9710c5779042
Tyler Johnson <tyler@builtmighty.com>
2024-01-05 16:44:36 +0000

    ✨ Add class for connecting to Jira

---
+++ classes/class-jira.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Jira.
+ * 
+ * Adds connectivity to Jira.
+ * 
+ * @package Built Mighty Kit
+ * @since   1.0.0
+ */
+class builtJira {
+
+    /**
+     * Variables.
+     * 
+     * @since   1.0.0
+     */
+    private $username;
+    private $api_key;
+
+    /**
+     * Construct.
+     * 
+     * @since   1.0.0
+     */
+    public function __construct() {
+
+        
+
+    }
+
+}
