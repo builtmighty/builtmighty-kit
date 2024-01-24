ce3d735b19c1df752623b81203963b2e79986d28
Tyler Johnson <tyler@builtmighty.com>
2024-01-24 16:34:10 +0000

    🐛 Fixing logic for activation methods

--- classes/class-admin.php
+++ classes/class-admin.php
@@ -82,7 +82,26 @@ class builtAdmin {
             </div><?php
 
             // Check for activation.
-            if( $_GET['activation'] !== 'true' ) { ?>
+            if( isset( $_GET['activation'] ) && $_GET['activation'] == 'true' ) { ?>
+
+                <div class="built-panel built-admin-panel">
+                    <h3 style="color:#fff;">Customer Email Protection</h3>
+                    <p>Run the following tool to replace email addresses in the database with custom generated, protected email addresses. Original addresses are stored and a tool is available to re-implement, if needed.</p>
+                    <div id="built-email-protect" class="built-tool">
+                        <div class="built-progress">
+                            <div class="built-bar-outer">
+                                <div class="built-bar-inner"></div>
+                            </div>
+                            <div class="built-bar-status"></div>
+                        </div>
+                        <div class="built-submit">
+                            <input type="submit" class="button built-action button-primary button-built" data-set='<?php echo json_encode( [ 'id' => 'built-email-protect', 'action' => 'built_email_protect', 'count' => 0, 'offset' => 0, 'total' => 0 ] ); ?>' name="built-tool" value="Run">
+                            <div class="built-loading"><?php include BUILT_PATH . 'assets/loading-icon.svg'; ?></div>
+                        </div>
+                    </div>
+                </div><?php
+
+            } else { ?>
 
                 <div class="built-panel built-admin-panel">
                     <p>Welcome to the client configuration panel for this client. Here, you can connect both the client's project on Jira, as well as their project manager.</p>
@@ -126,25 +145,6 @@ class builtAdmin {
                         </div>
                         <div class="built-submit">
                             <input type="submit" class="button built-action button-primary button-built" data-set='<?php echo json_encode( [ 'id' => 'built-email-tool', 'action' => 'built_email_replace', 'count' => 0, 'offset' => 0, 'total' => 0 ] ); ?>' name="built-tool" value="Run">
-                            <div class="built-loading"><?php include BUILT_PATH . 'assets/loading-icon.svg'; ?></div>
-                        </div>
-                    </div>
-                </div><?php
-
-            } else { ?>
-
-                <div class="built-panel built-admin-panel">
-                    <h3 style="color:#fff;">Customer Email Protection</h3>
-                    <p>Run the following tool to replace email addresses in the database with custom generated, protected email addresses. Original addresses are stored and a tool is available to re-implement, if needed.</p>
-                    <div id="built-email-protect" class="built-tool">
-                        <div class="built-progress">
-                            <div class="built-bar-outer">
-                                <div class="built-bar-inner"></div>
-                            </div>
-                            <div class="built-bar-status"></div>
-                        </div>
-                        <div class="built-submit">
-                            <input type="submit" class="button built-action button-primary button-built" data-set='<?php echo json_encode( [ 'id' => 'built-email-protect', 'action' => 'built_email_protect', 'count' => 0, 'offset' => 0, 'total' => 0 ] ); ?>' name="built-tool" value="Run">
                             <div class="built-loading"><?php include BUILT_PATH . 'assets/loading-icon.svg'; ?></div>
                         </div>
                     </div>
