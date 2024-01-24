609d1f002b974f1e77e88b19d18d6de77ba0a37b
Tyler Johnson <tyler@builtmighty.com>
2024-01-24 17:34:21 +0000

    ✨ Add a new method to Jira helper to sort issues

--- inc/class-jira-helper.php
+++ inc/class-jira-helper.php
@@ -58,4 +58,68 @@ class builtJiraHelper {
         
     }
 
+    /**
+     * Organize issues.
+     * 
+     * @since   1.0.0
+     */
+    public function sort_issues( $response ) {
+
+        // Set.
+        $issues = [];
+
+        // Loop through response.
+        foreach( $response['issues'] as $issue ) {
+
+            // Get status class.
+            $class = $this->issue_class( $issue['fields']['status']['name'] );
+
+            // Shorten status.
+            $issue['fields']['status']['name'] = str_replace( 'In Development', 'Dev', $issue['fields']['status']['name'] );
+
+            // Set.
+            $issues[$issue['key']] = [
+                'summary'   => $issue['fields']['summary'],
+                'status'    => $issue['fields']['status'],
+                'assignee'  => $issue['fields']['assignee'],
+                'class'     => $class,
+            ];
+
+        }
+
+        // Return.
+        return $issues;
+
+    }
+
+    /**
+     * Set issue class.
+     * 
+     * @since   1.0.0
+     */
+    public function issue_class( $status ) {
+
+        // Set statuses.
+        if( $status === 'Ready' ) {
+
+            // Set.
+            $class = 'is-ready';
+
+        } elseif( $status === 'Done' ) {
+
+            // Set.
+            $class = 'is-done';
+
+        } else {
+
+            // Set.
+            $class = 'is-dev';
+
+        }
+
+        // Return.
+        return $class;
+
+    }
+ 
 }
