1225422dd7dccd7a0a5e1450e892dc6ccbbdd041
Tyler Johnson <tyler@builtmighty.com>
2024-01-31 20:03:58 +0000

    (main) 🔖 Update version to bypass caching

--- builtmighty-kit.php
+++ builtmighty-kit.php
@@ -21,7 +21,7 @@ if( ! defined( 'WPINC' ) ) { die; }
  * 
  * @since   1.0.0
  */
-define( 'BUILT_VERSION', '1.1.0' );
+define( 'BUILT_VERSION', '1.1.1' );
 define( 'BUILT_NAME', 'builtmighty-kit' );
 define( 'BUILT_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
 define( 'BUILT_URI', trailingslashit( plugin_dir_url( __FILE__ ) ) );
