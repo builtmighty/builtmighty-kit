070c396ebc9ecacdddf70a56e4a4d50dfadd13e1
Tyler Johnson <tyler@builtmighty.com>
2024-02-01 21:17:15 +0000

    (main) 🐛 Fix bugs within GitHub dev display

--- classes/class-dev.php
+++ classes/class-dev.php
@@ -111,8 +111,18 @@ class builtDev {
         // Get Jira issues.
         echo $this->get_jira_issues();
 
-        // Get Git.
-        echo $this->get_git();
+        // Check for Git.
+        if( ! empty( $this->get_git_shell() ) ) {
+
+            // Get Git via shell.
+            echo $this->get_git_shell();
+
+        } else {
+
+            // Get basic Git.
+            echo $this->get_git();
+
+        }
 
         // Get plugin readme.
         echo $this->get_readme();
@@ -322,11 +332,11 @@ class builtDev {
     }
 
     /**
-     * Get Git.
+     * Get Git shell.
      * 
      * @since   1.0.0
      */
-    public function get_git() {
+    public function get_git_shell() {
 
         // Check if shell_exec exists.
         if( ! is_function( 'shell_exec' ) ) return;
@@ -389,6 +399,76 @@ class builtDev {
                 </p>
             </div><?php
 
+        }
+
+        // Return.
+        return ob_get_clean();
+
+    }
+
+    /**
+     * Get Git.
+     * 
+     * @since   1.0.0
+     */
+    public function get_git() {
+
+        // Start.
+        ob_start();
+
+        // Set path.
+        $git = ABSPATH . '/.git';
+
+        // Check if Git is installed.
+        if( is_dir( $git ) ) {
+
+            // Get remote origin from .git/config.
+            $config = file_get_contents( $git . '/config' );
+
+            // Get repo URL.
+            preg_match( '/url = (.*)/', $config, $matches );
+
+            // Get branch.
+            $branch = trim( str_replace( 'ref: refs/heads/', '', file_get_contents( $git . '/HEAD' ) ) );
+
+            // Check for matches.
+            if( $matches[1] ) {
+
+                // Set repo.
+                $repo = str_replace( '.git', '', $matches[1] );
+
+                // Set colors.
+                $colors = ( in_array( $branch, [ 'master', 'main', 'prod', 'production' ] ) ) ? ' style="background:green;"' : '';
+
+                // Output. ?>
+                <div class="built-panel">
+                    <p style="margin-top:0;">
+                        <strong>💻 GitHub</strong>
+                    </p>
+                    <ul style="margin:0;">
+                        <li>Branch: <code<?php echo $colors; ?>><?php echo $branch; ?></code></li>
+                    </ul>
+                    <p style="margin:0;">
+                        <a href="<?php echo $matches[1]; ?>" target="_blank" class="built-button" style="margin-top:10px;">View Repo</a>
+                    </p>
+                </div><?php
+
+            } else {
+
+                // Display message. ?>
+                <div class="built-panel">
+                    <p style="margin:0;"><strong>A Git repo is not setup.</strong> Create a Git repo to use this feature.</p>
+                </div><?php
+
+            }
+
+        } else {
+
+            // Display message. ?>
+            <div class="built-panel">
+                <p style="margin:0;"><strong>A Git repo is not setup.</strong> Create a Git repo to use this feature.</p>
+            </div><?php
+            
         }
 
         // Return.
