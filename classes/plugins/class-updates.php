0e8e24ff1ef1fa48a776bee1c18773f6996a311a
Tyler Johnson <tyler@builtmighty.com>
2024-04-25 21:29:47 +0000

    (LEA-79) ✨ Update messaging and information

--- classes/plugins/class-updates.php
+++ classes/plugins/class-updates.php
@@ -43,7 +43,7 @@ class builtUpdates {
                     <div class="builtmighty-kit-modal-content">
                         <span class="builtmighty-kit-modal-close">&times;</span>
                         <h2>WARNING: Updating Plugins/Themes</h2>
-                        <p>Updating plugins/themes on production can cause the site to crash and be inaccessible. Even if it does not crash the site, it brings uncommitted code onto the server and will cause the automated deployment system to fail, which will make deployments take much longer. Please only update if absolutely necessary.</p>
+                        <p>Updating plugins/themes on production can cause the site to crash and be inaccessible. Even if it does not crash the site, it brings uncommitted code onto the server and will cause the automated deployment system to fail, which will make deployments take much longer. Please only update if absolutely necessary. If you have any questions, please reach out to <a href="mailto:<?php echo antispambot( 'developers@builtmighty.com', true ); ?>">Built Mighty</a>.</p>
                         <div class="builtmighty-kit-modal-buttons">
                             <button class="button button-primary" id="builtmighty-kit-update">Update</button>
                             <button class="button button-secondary" id="builtmighty-kit-close">Close</button>
