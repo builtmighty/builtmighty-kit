5cea72d8a46c10fda7d62804beef9f7b91b58b01
Tyler Johnson <tyler@builtmighty.com>
2024-05-27 01:12:18 +0000

    (2fa) ✨ Save the authenticator field

--- classes/security/class-2fa.php
+++ classes/security/class-2fa.php
@@ -16,9 +16,11 @@ class built2FA {
      */
     public function __construct() {
 
-        // Show.
+        // Fields.
         add_action( 'show_user_profile', [ $this, 'authenticator_field' ] );
         add_action( 'edit_user_profile', [ $this, 'authenticator_field' ] );
+        add_action( 'personal_options_update', [ $this, 'save_authenticator_field' ] );
+        add_action( 'edit_user_profile_update', [ $this, 'save_authenticator_field' ] );
         
     }
 
@@ -42,6 +44,23 @@ class built2FA {
             </tr>
         </table><?php
 
-    }   
+    }
+    
+    /**
+     * Save Authenticator field.
+     * 
+     * Save authenticator field to user profile.
+     * 
+     * @since   2.0.0
+     */
+    public function save_authenticator_field( $user_id ) {
+
+        // Check if current user can edit user.
+        if( ! current_user_can( 'edit_user', $user_id ) ) return;
+
+        // Update user meta.
+        update_user_meta( $user_id, 'google_authenticator_secret', $_POST['google_authenticator_secret'] );
+
+    }
 
 }
