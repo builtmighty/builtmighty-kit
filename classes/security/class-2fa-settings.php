5fbf01ee5c41147c96b1c95163202a55b93debfb
Tyler Johnson <tyler@builtmighty.com>
2024-05-28 20:04:00 +0000

    (2fa-settings) ✨ Add new 2FA settings class

---
+++ classes/security/class-2fa-settings.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * 2FA Settings.
+ * 
+ * Requires 2FA for saving important settings.
+ * 
+ * @package Built Mighty Kit
+ * @since   2.0.0
+ */
+class built2FASettings {
+
+    /**
+     * Construct.
+     * 
+     * @since   2.0.0
+     */
+    public function __construct() {
+
+        
+
+    }
+
+}
