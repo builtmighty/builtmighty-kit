019dbe592f9edad05388a9203be46c896cc9dc91
Tyler Johnson <tyler@builtmighty.com>
2024-05-30 10:56:25 +0000

    (2fa-settings) ✨ Move authentication to a dedicated class

---
+++ classes/security/class-auth.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Authenticate 2FA.
+ * 
+ * Run authentication against 2FA codes.
+ * 
+ * @package Built Mighty Kit
+ * @since   2.0.0
+ */
+use Sonata\GoogleAuthenticator\GoogleAuthenticator;
+class builtAuth {
+
+    /**
+     * Authenticate.
+     * 
+     * Authenticate user.
+     * 
+     * @since   2.0.0
+     */
+    public function authenticate( $user_id, $code ) {
+
+        // Check if setup.
+        if( empty( get_user_meta( $user_id, 'google_authenticator_confirmed', true ) ) ) return true;
+
+        // Check for secret.
+        if( empty( get_user_meta( $user_id, 'google_authenticator_secret', true ) ) ) return true;
+
+        // Get secret.
+        $secret = get_user_meta( $user_id, 'google_authenticator_secret', true );
+
+        // Check for code.
+        if( isset( $code ) ) {
+
+            // Get Google Authenticator.
+            $google = new GoogleAuthenticator();
+
+            // Sanitize the code.
+            $code = sanitize_text_field( $code );
+
+            // Check code.
+            if( $google->checkCode( $secret, $code ) ) {
+
+                // Return true.
+                return true;
+
+            } else {
+
+                // Return false.
+                return false;
+
+            }
+
+        }
+
+        // Return false.
+        return false;
+
+    }
+
+}
