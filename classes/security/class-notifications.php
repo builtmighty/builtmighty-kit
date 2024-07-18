976ce7c584c3ccb322e7c72bb0b14dcbe8a5bd5a
Tyler Johnson <tyler@builtmighty.com>
2024-07-18 14:22:46 +0000

    (2.2.0) ✨ Create notifications class

---
+++ classes/security/class-notifications.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Notifications.
+ * 
+ * Send Slack notifications about certain touchy operations.
+ * 
+ * @package Built Mighty Kit
+ * @since   1.0.0
+ */
+namespace BuiltMightyKit\Security;
+class builtNotifications {
+
+}
