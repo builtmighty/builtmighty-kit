8c32baf00dbc6beac52b2bb6181e9e3d14cf9e94
Tyler Johnson <tyler@builtmighty.com>
2024-08-15 17:33:46 +0000

    (LEA-126) ✨ Add error message to confirmation page

--- views/security/security-confirm.php
+++ views/security/security-confirm.php
@@ -5,12 +5,16 @@
  * @package Built Mighty Kit
  * @since   2.0.0
  */ 
-echo '<pre>';
-print_r( $_POST );
-echo '</pre>';
-?>
+// Check for error.
+if( isset( $_GET['status'] ) && $_GET['status'] == 'error' ) {
+
+    // Output error. ?>
+    <p class="built-security-error">The code you entered is incorrect. Please try again.</p><?php
+    
+} ?>
 <form method="post" action="<?php echo site_url( '/security?key=' . $_GET['key'] . '&confirm=true' ); ?>">
     <div class="built-panel-code">
+        <input type="hidden" name="key" value="<?php echo $_GET['key']; ?>" />
         <input type="text" inputmode="numeric" maxlength="6" name="google_authenticator_code" id="google_authenticator_code" class="regular-text" placeholder="Enter your code" />
     </div>
     <div class="built-security-actions">
