2ee26cb6daa0e5e2196e404785736246273b40d0
Tyler Johnson <tyler@builtmighty.com>
2025-04-09 10:06:34 -0400

    :gear: Update external API requests block
    Signed-off-by: Tyler Johnson <tyler@builtmighty.com>

--- public/class-block-external.php
+++ public/class-block-external.php
@@ -20,7 +20,7 @@ class block_external {
     public function __construct() {
 
         // Check.
-        if( ( empty( get_option( 'kit_block_external' ) ) && is_kit_mode() ) || get_option( 'kit_block_external' ) == 'disable' ) return;
+        if( empty( get_option( 'kit_block_external' ) && is_kit_mode() ) || get_option( 'kit_block_external' ) == 'disable' ) return;
 
         // Filter.
         add_filter( 'pre_http_request', [ $this, 'block' ], 10, 3 );
